50e04aece1200a72e5743b73ea10152acd13597a
Cameron-Witcher <CameronWitcher1@gmail.com>
2022-12-10 17:41:14 -0600

    test

--- src/main/java/net/lushmc/core/utils/PlayerSkull.java
+++ src/main/java/net/lushmc/core/utils/PlayerSkull.java
@@ -3,7 +3,6 @@ package net.lushmc.core.utils;
 import java.lang.reflect.Field;
 import java.util.UUID;
 
-import org.apache.commons.codec.binary.Base64;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.inventory.ItemStack;
@@ -32,8 +31,7 @@ public class PlayerSkull {
 			meta.setOwningPlayer(player);
 		else {
 			GameProfile profile = new GameProfile(UUID.randomUUID(), null);
-			byte[] encodedData = Base64.encodeBase64(String.format("{textures:{SKIN:{url:\"%s\"}}}", url).getBytes());
-			profile.getProperties().put("textures", new Property("textures", new String(encodedData)));
+			profile.getProperties().put("textures", new Property("textures", url));
 			Field profileField = null;
 
 			try {
